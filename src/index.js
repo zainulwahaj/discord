68bcbf37df312744d964e89b88baf0f4e4464606
zainulwahaj <zainulwahaj@gmail.com>
2026-04-11 22:09:17 +0500

    Refactor config and index to support conditional message content intent

--- src/index.js
+++ src/index.js
@@ -3,17 +3,22 @@ import { Client, GatewayIntentBits, Partials } from 'discord.js';
 import { handleMessage } from './bot.js';
 import { config } from './config.js';
 
+const intents = [
+  GatewayIntentBits.Guilds,
+  GatewayIntentBits.GuildMessages,
+  GatewayIntentBits.DirectMessages,
+];
+
+if (config.enableMessageContentIntent) {
+  intents.push(GatewayIntentBits.MessageContent);
+}
+
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.DirectMessages,
-  ],
+  intents,
   partials: [Partials.Channel],
 });
 
-client.once('ready', () => {
+client.once('clientReady', () => {
   console.log(`[${config.botName}] Logged in as ${client.user.tag}`);
   console.log(`[${config.botName}] Watching ${client.guilds.cache.size} server(s)`);
 });
